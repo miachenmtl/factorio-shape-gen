49ffaf33d16b06e3f96b39aba0bdc426cd566903
Mia Chen <mia.chen.mtl@gmail.com>
2021-02-15 19:06:29 -0500

    Create CollapsedText component
    Create a component that is a clickable div with button role that shows and hides text, to be used in the next commit.

---
+++ src/components/CollapsedText.js
@@ -0,0 +1,34 @@
+import { Fragment, h } from 'preact';
+import { useState } from 'preact/hooks';
+
+// See https://www.w3.org/TR/wai-aria-practices-1.1/examples/accordion/accordion.html
+// https://dev.to/stereobooster/accessible-react-accordion-component-4p99
+
+export default function CollapsedText({ label, text }) {
+  const [expanded, setExpanded] = useState(false);
+  const idPrefix = label.toLowerCase(); // TODO: remove whitespace, or truncate after first word
+  const expanderId = `${idPrefix}-expander`;
+  const textId = `${idPrefix}-text`;
+
+  return (
+    <>
+      <div
+        id={expanderId}
+        role="button"
+        aria-expanded={expanded}
+        aria-controls={textId}
+        onClick={() => { setExpanded(!expanded); }}
+      >
+        {label}
+      </div>
+      <p
+        id={textId}
+        aria-labelledby="about-expander"
+        foo="bar"
+        hidden={!expanded}
+      >
+        {text}
+      </p>
+    </>
+  );
+}
