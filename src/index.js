832c675c4fb6ac93e47c1b91663bc58bc511c36c
Mia Chen <mia.chen.mtl@gmail.com>
2021-04-22 17:34:03 -0400

    Rearrange and style UI elements and page layout

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import Header from './sections/Header';
 import Main from './sections/Main';
 import Footer from './sections/Footer';
 
-import './style.css';
+import style from './components/style.css';
 
 export default function App() {
 	return (
-		<div>
+		<div className={style.container}>
 			<Header />
 			<Main />
 			<Footer />
